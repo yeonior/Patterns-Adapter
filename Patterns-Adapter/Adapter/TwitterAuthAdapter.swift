ef7e5b11817aa144bc7d45c43ca51da113269856
Rus <de.mr44@gmail.com>
2022-01-11 19:22:06 +0500

    Added TwitterAuthAdapter class

---
+++ Patterns-Adapter/Adapter/TwitterAuthAdapter.swift
@@ -0,0 +1,31 @@
+//
+//  TwitterAuthAdapter.swift
+//  Patterns-Adapter
+//
+//  Created by Ruslan on 11.01.2022.
+//
+
+import Foundation
+
+class TwitterAuthAdapter: AuthService {
+    
+    private var auth = TwitterAuth()
+    
+    public func login(email: String,
+                      password: String,
+                      success: @escaping (User, Token) -> Void,
+                      failure: @escaping (Error?) -> Void) {
+        
+        auth.login(email: email, password: password) { currentUser, error in
+            
+            guard let currentUser = currentUser else {
+                failure(error)
+                return
+            }
+            
+            let user = User(email: currentUser.email, password: currentUser.password)
+            let token = Token(token: currentUser.token)
+            success(user, token)
+        }
+    }
+}
